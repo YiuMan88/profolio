53ea9e5d99a0c770c48f0dfd84def983be528b06
Francis <yaowen021316@hotmail>
2021-11-07 18:02:44 +1100

    feat()responsive fixed

--- js/index.js
+++ js/index.js
@@ -3,15 +3,16 @@ const submenuIcon =
 const submenu = document.getElementsByClassName('profolio__mobilenavwrapper')[0]
 const closesubmenu =
 	document.getElementsByClassName('profolio__closesub')[0] || null
-
+const subMenuItems = document.getElementsByClassName('profolio__mobilenav')[0]
 if (submenuIcon) {
 	submenuIcon.addEventListener('click', () => {
 		submenu.style.display = 'block'
+		subMenuItems.style.animation = 'fadeInRight 1s  forwards'
 	})
 }
 if (closesubmenu) {
 	closesubmenu.addEventListener('click', () => {
-		submenu.style.display = 'none'
+		submenu.style.animation = 'fadeOutRight 1s  forwards'
 	})
 }
 ScrollOut({
